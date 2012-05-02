8c69ed92545da14fdc7a41e51d15d98f79093a70
theartofweb <duncan@chirp.com.au>
2012-05-02 14:27:16 +0300

    changes to cookie handling avoid stale cookies; minor html/css cleanup; not tested in wild

--- src/cprompt.js
+++ src/cprompt.js
@@ -1,10 +1,10 @@
 /**
 	Simple Cookie Prompt
 	Idea: @panda_doodle - Coded: @michaelw90
+
+	Forked: @theartofweb
 **/
 var cPrompt = {
-
-	cookie: false,
 
 	init: function(){
 		if(!this.checkCookie()){
@@ -13,30 +13,26 @@ var cPrompt = {
 	},
 
 	checkCookie: function(){
-		if(document.cookie.match(/cPrompt_useCookies=1/)){
-			this.cookie = true;
-		}
-		return this.cookie;
+		return document.cookie.match(/cPrompt_useCookies=1/) && this.saveCookie();
 	},
 
 	loadPrompt: function(){
-		var holder = document.createElement('div');
-		holder.innerHTML = "<div style='margin: 10px;'>This site would like to place cookies on your computer to help make this website better. To find out more about cookies, visit <a href='http://en.wikipedia.org/wiki/HTTP_cookie' style='color: #FFF;'>Wikipedia</a>.<br /><input type='checkbox' onclick='cPrompt.clickEvent(this);' id='cPrompt_check'> <label for='cPrompt_check'>I accept cookies from this site.</label></div>"
-		holder.style.cssText = 'background: #000; position: absolute; width: 100%; color: #FFF; font-family: verdana; font-size: 11px; opacity: 0.85';
-		holder.id = 'cPrompt_Holder';
-		var b = document.getElementsByTagName('body')[0];
-		b.insertBefore(holder, b.firstChild);
+		var holder = document.createElement("div");
+		holder.innerHTML = "This site would like to place cookies on your computer to help make this website better. To find out more about cookies, visit <a href='http://en.wikipedia.org/wiki/HTTP_cookie' style='color: #FFF;'>Wikipedia</a>.<br /><input type='checkbox' onclick='cPrompt.clickEvent(this);' id='cPrompt_check'><label for='cPrompt_check'>I accept cookies from this site.</label>";
+		holder.style.cssText = "position: absolute; padding: 10px; background: rgba(0,0,0,0.85); font-family: sans-serif; font-size: 11px; color: rgba(255,255,255,0.85);";
+		holder.id = "cPrompt_Holder";
+		var b = document.getElementsByTagName("body")[0];
+		b.insertBefore(holder, b.firstChild)
 	},
 
 	saveCookie: function(){
-		document.cookie = "cPrompt_useCookies=1;expires=" + (new Date()).toGMTString().replace(/\d{4}/, '2050');
+		document.cookie = "cPrompt_useCookies=1;expires=" + (new Date()).toGMTString().replace(/(\d{4})/, function(x){ return Number(x)+1 })
+		return true;
 	},
 
 	clickEvent: function(obj){
-		if(obj.checked){
-			this.saveCookie();
-			document.getElementById('cPrompt_Holder').style.display = 'none';
-		}
+		this.saveCookie();
+		document.getElementById("cPrompt_Holder").style.display = "none"
 	}
 }
 if(document.addEventListener){
